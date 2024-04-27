17bcdfa322c0fa05cef75c371101171a2c1620cc
Bluethefox <47904806+bluethefoxofficial@users.noreply.github.com>
2024-04-27 17:27:11 +0100

    baton

--- src/Baton_server/index.js
+++ src/Baton_server/index.js
@@ -1,63 +1,76 @@
 const express = require('express');
 const fs = require('fs').promises;
 const path = require('path');
+const { config } = require('process');
+const { generateHTMLPage, generatesearchpageHTMLPage } = require('./pages');
 const app = express();
 const port = 3000;
 
+//allow downloading of files from packages
+app.use('/packages', express.static(path.join(__dirname, 'packages')));
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+
+
+
+//search packages with a search bar and find packages with their name and description and base it off the keywords in the bpackage.json of each package
+app.get('/search', async (req, res) => {
+  console.log("searching");
+  const packagesDir = path.join(__dirname, 'packages');
+  const packages = await fs.readdir(packagesDir);
+  const jsonpackages = [];
+  const searchQuery = req.query.q; // Get the search query from the request query parameters
+  for (const packageName of packages) {
+    const packagePath = path.join(packagesDir, packageName, 'bpackage.json');
+    const packageJson = await fs.readFile(packagePath);
+    const packjson = JSON.parse(packageJson);
+    // Check if the package name or description contains the search query
+    if (packjson.name.includes(searchQuery) || packjson.description.includes(searchQuery)) {
+      // Check if the package keywords contain the search query
+      if (packjson.keywords.includes(searchQuery)) {
+        jsonpackages.push(packjson);
+      }
+    }
+  }
+  //get config file in config/batonpm.json
+  const configPath = path.join(__dirname, 'config', 'batonpm.json');
+  const configData =  await fs.readFile(configPath);
+  const configJson = JSON.parse(configData);
+  if (jsonpackages.length === 0) {
+    res.type('html').send('No search results found');
+  } else {
+    res.type('html').send(generatesearchpageHTMLPage(jsonpackages,configJson));
+  }
+});
+app.get('/packages/', async (req, res) => {
+
+
+  res.type('html').send('Please enter a package name in the URL or goto <a href="/search">/search</a> to search for packages');
+
+});
 app.get('/packages/:packageName', async (req, res) => {
   const packageName = req.params.packageName;
-  const packagePath = path.join(__dirname, 'packages', packageName, 'package.json');
-
+  const packagePath = path.join(__dirname, 'packages', packageName, 'bpackage.json');
+  const packageJson = await fs.readFile(packagePath);
+  const packjson = JSON.parse(packageJson);
+       //get config file in config/batonpm.json
+       const configPath = path.join(__dirname, 'config', 'batonpm.json');
+       const configData =  await fs.readFile(configPath);
+       const configJson = JSON.parse(configData);
   try {
-    const packageData = await fs.readFile(packagePath, 'utf8');
-    const packageJson = JSON.parse(packageData);
-
-    // Respond differently based on the "Accept" header
     if (req.accepts('html')) {
-      res.type('html').send(generateHTMLPage(packageJson));
+      res.type('html').send(generateHTMLPage(packjson,configJson));
     } else {
-      res.json(packageJson);
+      res.json(packjson);
     }
   } catch (error) {
-    res.status(404).send(`Package not found: ${packageName}`);
+    console.error("\x1b[31m"+error+"\x1b[0m");
+    res.status(404).send(`<html><head><link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura-dark.css" type="text/css"></head><body>Package not found: ${packageName}</body></html>`);
   }
 });
 
-function generateHTMLPage(packageJson) {
-  return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>${packageJson.name} Info</title>
-    </head>
-    <body>
-      <h1>${packageJson.name}</h1>
-      <p><strong>Description:</strong> ${packageJson.description}</p>
-      <p><strong>Version:</strong> ${packageJson.version}</p>
-      <ul>
-        <strong>Downloads:</strong>
-        ${packageJson.files.map(file => `<li><a href="${file}">Download</a></li>`).join('')}
-      </ul>
-    </body>
-    </html>
-  `;
-}
-
-function generateJSONFile(packageJson) {
-    return JSON.stringify(packageJson, null, 2);
-    }
-    
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
   //list first 5 packages
-    fs.readdir(path.join(__dirname, 'packages'))
-        .then(packages => {
-        console.log('Available packages:');
-        packages.slice(0, 5).forEach(packageName => {
-            console.log(`- ${packageName} (http://localhost:${port}/packages/${packageName})`);
-        });
-        });
 });
---
+++ src/Baton_server/pages.js
@@ -0,0 +1,146 @@
+function generateHTMLPage(packageJson,configJson) {
+  
+    const navbarLinks = configJson.navbar.map(link => `<a href="${link.url}">${link.title}</a>`).join(' | ');
+  
+  
+    return `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>${packageJson.name} Info - Baton package manager</title>
+        <link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura-dark.css" type="text/css">
+        <style>
+        body {
+          max-width: 70em;
+        }
+        
+        nav {
+          display: flex;
+          justify-content: space-between;
+        }
+        input {
+          width: 100%;
+        }
+        
+        footer {
+          border-top: 1px solid gray;
+        }
+        </style>      
+  
+        <header>
+        <nav>
+          <div>${configJson.sitename}</div>
+  
+          <div>
+            ${navbarLinks}
+          </div>
+        </nav>
+      </header>
+      <body>
+        <h1>${packageJson.name}</h1>
+        <p><strong>Description:</strong> ${packageJson.description}</p>
+        <p><strong>Version:</strong> ${packageJson.version}</p>
+        <p><strong>Author:</strong> ${packageJson.author}</p>
+        <ul>
+          <strong>Downloads:</strong>
+          ${packageJson.files.map(file => `<li><a href="/packages/${packageJson.name}/${file}">Download ${file}</a></li>`).join('')}
+        <l>
+  
+        <script src="https://cdn.jsdelivr.net/npm/highlight.js"></script>
+          <script>hljs.initHighlightingOnLoad();</script>
+  
+      <h1>Dependencies</h1>
+      <ul>
+        ${Object.keys(packageJson.dependencies).map(dep => `<li>${dep}</li>`).join('')}
+      </ul>
+  
+      <h1>Eling Version support</h1>
+      <ul>
+        ${Object.keys(packageJson.eling).map(eling => `<li>${eling}</li>`).join('')}
+      </ul>
+
+      <h1> keywords </h1>
+        <ul>
+            ${packageJson.keywords.map(keyword => `<li>${keyword}</li>`).join('')}
+        </ul>
+
+      <h1>Baton install command</h1>
+        <pre><code>baton install ${packageJson.name}</code></pre>
+  
+      <h2>Package JSON raw</h2>
+      <pre><code>${generateJSONFile(packageJson)}</code></pre>
+  
+      </body>
+      </html>
+    `;
+  }
+
+  function generaterandompackageHTMLPage(configJson) {
+
+  
+
+  }
+
+  function generatesearchpageHTMLPage(jsonpackages,configJson) {
+        //generate a html page with a search bar and a list of packages with their name and description and when clicked on the name it will redirect to the package page
+        const navbarLinks = configJson.navbar.map(link => `<a href="${link.url}">${link.title}</a>`).join(' | ');
+        return `
+        <!DOCTYPE html>
+        <html lang="en">
+        
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>${configJson.sitename}</title>
+          <link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura-dark.css" type="text/css">
+          <style>
+          body {
+            max-width: 70em;
+          }
+          
+          nav {
+            display: flex;
+            justify-content: space-between;
+          }
+          input {
+            width: 100%;
+          }
+          
+          footer {
+            border-top: 1px solid gray;
+          }
+          </style>      
+        
+          <header>
+          <nav>
+            <div>${configJson.sitename}</div>
+        
+            <div>
+              ${navbarLinks}
+            </div>
+          </nav>
+        </header>
+
+        <body>
+          <h1>${configJson.sitename}</h1>
+          <form action="/search" method="get">
+            <input type="text" name="q" placeholder="Search packages">
+            <input type="submit" value="Search">
+          </form>
+          <ul>
+            ${jsonpackages.map(packageJson => `<li><a href="/packages/${packageJson.name}">${packageJson.name}</a> - ${packageJson.description}</li>`).join('')}
+          </ul>
+        </body>
+        </html>
+        `;
+
+
+        }
+  function generateJSONFile(packageJson) {
+    return JSON.stringify(packageJson, null, 2);
+    }
+
+
+  module.exports = { generateHTMLPage, generatesearchpageHTMLPage};
